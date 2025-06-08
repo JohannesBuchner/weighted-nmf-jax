0fd8893bfc51c35ef35d6aaad3d381f3b8fae26d
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-06-08 22:39:17 +0200

    rename package to wNMFx; document, code format

--- setup.py
+++ setup.py
@@ -3,33 +3,37 @@
 import setuptools
 from os import path
 
-
-
-## Open README
+# Open README
 here = path.abspath(path.dirname(__file__))
-readme_path = path.join(here, 'README.md')
-with open(readme_path, 'r') as f:
+readme_path = path.join(here, "README.md")
+with open(readme_path, "r") as f:
     readme = f.read()
 
 setuptools.setup(
-            name='wNMF',
-            version='0.0.42',
-            long_description=readme,
-            description='wNMF: weighted Non-Negative matrix Factorization',
-            long_description_content_type='text/markdown',
-            author='SN',
-            author_email='scottnanda@gmail.com',
-            url='https://github.com/asn32/weighted-nmf',
-            license='MIT License',
-            packages=['wNMF'],
-            python_requires='>=3.6',
-            install_requires='numpy>=1.13',
-            include_package_data=False,
-            classifiers=[
-                "Programming Language :: Python",
-                "Programming Language :: Python :: 3",
-                "Programming Language :: Python :: 3.6",
-                "Topic :: Scientific/Engineering :: Bio-Informatics",
-                "Topic :: Scientific/Engineering :: Mathematics"
-            ]
+    name="wNMFx",
+    version="0.1.0",
+    long_description=readme,
+    description="wNMFx: weighted Non-Negative matrix Factorization in jax",
+    long_description_content_type="text/rst",
+    author="JohannesBuchner",
+    author_email="johannes.buchner.acad@gmx.com",
+    url="https://github.com/JohannesBuchner/weighted-nmf-jax",
+    license="MIT License",
+    packages=["wNMFx"],
+    python_requires=">=3.6",
+    install_requires="numpy>=1.13",
+    include_package_data=False,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
 )
--- tests/test_wNMF.py
+++ tests/test_wNMF.py
@@ -1,55 +1,72 @@
 import numpy as np
-from wNMF import wNMF
+from wNMFx import wNMF
 import matplotlib.pyplot as plt
 
-def test_random():
+def test_random(plot=False):
     ## An example on simulated data
     n = 101
     features = 100
     components = 4
+    n_run = 20
+    max_iter = 4000
+    np.random.seed(123)
     
     shapes_true = np.array([0.5 + 0.5 * np.sin(np.arange(features) / 10 / 2**i + np.random.uniform(0, np.pi)) for i in range(components)])
     shapes_true[0] = 1
     shapes_true[1] = np.exp(-np.arange(features) / 400)
 
-    plt.plot(shapes_true.T);
-    plt.savefig('test_normal_input.pdf')
-    plt.close()
+    if plot:
+        plt.plot(shapes_true.T);
+        plt.savefig('test_normal_input.pdf')
+        plt.close()
 
-    X = np.random.normal(100 * np.random.uniform(size=(n, components)) @ shapes_true, 1)
+    X = np.abs(np.random.normal(1 * (10**np.random.uniform(-1, 1, size=(n, components)) @ shapes_true), 1))
+    assert X.shape == (n, features)
     W = np.ones_like(X)
-    plt.plot(X[:10].T, ls=' ', marker='o');
-    plt.savefig('test_normal_data.pdf')
-    plt.close()
+    if plot:
+        plt.plot(X[:10].T, ls=' ', marker='o');
+        plt.savefig('test_normal_data.pdf')
+        plt.close()
+        fig_components, ax_components = plt.subplots()
+        fig_loss, ax_loss = plt.subplots()
 
-    model = wNMF(n_components=components, beta_loss='frobenius', max_iter=1000, track_error=True, verbose=2)
-    fit = model.fit(X=X, W=W, n_run=5)
-    print(fit.V.shape)
-    print(fit.U.shape)
-    assert fit.V.shape == (components, features)
-    assert fit.U.shape == (n, components)
-    assert np.shape(fit.err) == ()
-    assert np.shape(fit.err) == ()
-    assert len(fit.error_tracker) == 5
-    assert len(fit.error_tracker[0]) == 1000
+    for init, ls in ('random', '-'), :#('sample', '--'):
+        model = wNMF(
+            n_components=components, beta_loss='frobenius', 
+            max_iter=max_iter, track_error=True, verbose=1, init=init, 
+            n_run=n_run)
+        fit = model.fit(X=X, W=W)
+        print(fit.V.shape)
+        print(fit.U.shape)
+        assert fit.V.shape == (components, n)
+        assert fit.U.shape == (features, components)
+        assert np.shape(fit.err) == ()
+        assert np.shape(fit.err) == ()
+        assert len(fit.error_tracker) == n_run
+        assert len(fit.error_tracker[0]) == max_iter
 
-    plt.plot(fit.V.T);
-    plt.savefig('test_normal.pdf')
-    plt.close()
+        if plot:
+            ax_components.plot(fit.U, ls=ls)
+            for i, err_tracked in enumerate(fit.error_tracker):
+                ax_loss.plot(err_tracked, label=f'run {i} init {init}',
+                ls=ls)
 
-    for i, err_tracked in enumerate(fit.error_tracker):
-        plt.plot(err_tracked, label=f'run {i}');
-    plt.yscale('log')
-    plt.legend()
-    plt.savefig('test_normal_loss.pdf')
-    plt.close()
+    if plot:
+        fig_components.savefig('test_normal.pdf')
+        plt.close(fig_components)
+        ax_loss.set_yscale('log')
+        ax_loss.legend()
+        fig_loss.savefig('test_normal_loss.pdf')
+        plt.close(fig_loss)
 
-def test_poisson():
+
+def test_poisson(plot=False):
     ## An example on simulated data
     n = 101
     features = 1000
     components = 4
-    n_run = 1
+    n_run = 10
+    max_iter = 2000
     np.random.seed(1)
 
     shapes_true = np.array([0.5 + 0.5 * np.sin(np.arange(features) / 10 / 2**i + np.random.uniform(0, np.pi)) for i in range(components)])
@@ -57,45 +74,57 @@ def test_poisson():
     shapes_true[1] = np.exp(-np.arange(features) / 400)
     shapes_true /= shapes_true.max(axis=0, keepdims=True)
 
-    for k, component in enumerate(shapes_true):
-        plt.plot(component, label=f'component {k}')
-    plt.legend()
-    plt.savefig('test_poisson_input.pdf')
-    plt.close()
+    if plot:
+        for k, component in enumerate(shapes_true):
+            plt.plot(component, label=f'component {k}')
+        plt.legend()
+        plt.savefig('test_poisson_input.pdf')
+        plt.close()
 
     ## An example on simulated data
-    X = 1. * np.random.poisson(100 * (10**np.random.uniform(-4, 2, size=(n, components))) @ shapes_true)
+    X = 1. * np.random.poisson(10 * (10**np.random.uniform(-4, 2, size=(n, components))) @ shapes_true)
     W = np.ones_like(X)
-    plt.plot(X[:10].T, ls=' ', marker='o');
-    plt.savefig('test_poisson_data.pdf')
-    plt.close()
+    assert X.shape == (n, features)
+    if plot:
+        plt.plot(X[:10].T, ls=' ', marker='o');
+        plt.savefig('test_poisson_data.pdf')
+        plt.close()
+        fig_components, ax_components = plt.subplots()
+        fig_loss, ax_loss = plt.subplots()
 
-    model = wNMF(n_components=components, beta_loss='kullback-leibler', max_iter=1000, track_error=True, verbose=2)
-    fit = model.fit(X=X, W=W, n_run=n_run)
+    for init, ls in ('random', '-'),:# , ('sample', '--'):
+        model = wNMF(
+            n_components=components, beta_loss='kullback-leibler', 
+            max_iter=max_iter, track_error=True, verbose=1, init=init, 
+            n_run=n_run)
+        fit = model.fit(X=X, W=W)
+        print(fit.V.shape)
+        print(fit.U.shape)
+        assert fit.V.shape == (components, n)
+        assert fit.U.shape == (features, components)
+        assert np.shape(fit.err) == ()
+        assert np.shape(fit.err) == ()
+        assert len(fit.error_tracker) == n_run
+        assert len(fit.error_tracker[0]) == max_iter
 
-    assert fit.V.shape == (components, features)
-    assert fit.U.shape == (n, components)
-    assert np.shape(fit.err) == ()
-    assert np.shape(fit.err) == ()
-    assert len(fit.error_tracker) == n_run
-    assert len(fit.error_tracker[0]) == 1000
+        if plot:
+            ax_components.plot(fit.U, ls=ls)
+            for i, err_tracked in enumerate(fit.error_tracker):
+                ax_loss.plot(err_tracked, label=f'run {i} init {init}',
+                ls=ls)
 
-    for k, component in enumerate(fit.V):
-        plt.plot(component, label=f'component {k}')
-    plt.legend()
-    plt.savefig('test_poisson.pdf')
-    plt.close()
+    if plot:
+        fig_components.savefig('test_poisson.pdf')
+        plt.close(fig_components)
+        ax_loss.set_yscale('log')
+        ax_loss.legend()
+        fig_loss.savefig('test_poisson_loss.pdf')
+        plt.close(fig_loss)
 
-    for i, err_tracked in enumerate(fit.error_tracker):
-        plt.plot(err_tracked, label=f'run {i}', lw=5 if np.argmin(fit.err_all) == i else 1);
-    plt.yscale('log')
-    plt.legend()
-    plt.savefig('test_poisson_loss.pdf')
-    plt.close()
 
 if __name__ == '__main__':
     import sys
     if sys.argv[1] == 'poisson':
-        test_poisson()
+        test_poisson(plot=True)
     else:
-        test_random()
+        test_random(plot=True)
